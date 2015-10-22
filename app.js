a4735a679415cb9ee34336b50fb02748d6e8d69c
arubinofaux <arubinofaux@gmail.com>
2015-10-22 16:42:12 -0400

    coje

--- app.js
+++ app.js
@@ -4,14 +4,29 @@ var cheerio = require('cheerio');
 
 var url = "http://www.artmiamifair.com/desktopdefault.aspx?tabid=17";
 
-
-request(url, function(error, response, html){
-  if(!error){
+request(url, function(error, response, html) {
+  if (!error) {
 
     var $ = cheerio.load(html);
 
-    var ttt = $("#_ctl7_dlDealersByName > tbody > tr > td > h1").text();
+    $('.LinkDealer').each(function(i, elem) {
+      var name = $(this).text();
+      var link = "http://www.artmiamifair.com/"+$(this).attr('href');
 
-    console.log(ttt);
+      request(link, function(error, response, html) {
+        if (!error) {
+          var $ = cheerio.load(html);
+
+          email = $('table#_ctl7_dlDealOnFair tr#_ctl7_dlDealOnFair__ctl0__ctl0_trMail .linkdealer a').attr('href').replace('mailto:','');
+          website = $('table#_ctl7_dlDealOnFair tr#_ctl7_dlDealOnFair__ctl0__ctl0_trURL .linkdealer a').attr('href');
+
+          console.log(name);
+          console.log(email);
+          console.log(website);
+          console.log('------------------');
+        }
+      });
+    });
+
   }
 })
